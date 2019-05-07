16aae164360cdafec2b9715f3f435c890f7bd974
Oleg K <brunneng9@gmail.com>
2019-05-07 16:20:32 +0300

    Task7_Countries

--- Task7_EU_Countries/Program.cs
+++ Task7_EU_Countries/Program.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task7_EU_Countries
 {
 	class Program
 	{
+		static readonly string textFile = @"C:\Work\Countries.txt";
+
 		static void Main(string[] args)
 		{
+			if (File.Exists(textFile))
+			{
+				string[] readFile = File.ReadAllLines(textFile);
+				Dictionary<int, Country> countries = new Dictionary<int, Country>();
+				for (int i = 0; i < readFile.Length; i++)
+				{
+					Country testCountry = new Country();
+					testCountry.Name = readFile[i];
+					testCountry.IsTelenorSupported = false;
+					countries.Add(i, testCountry);
+				}
+				Country ukraine = new Country();
+				ukraine.Name = "Ukraine";
+				countries.Add((countries.Keys.Max() + 1), ukraine);
+				foreach (KeyValuePair<int, Country> keyValue in countries)
+				{
+					if ((keyValue.Value.Name) == "Denmark" || (keyValue.Value.Name) == "Hungary")
+					{
+						countries[keyValue.Key].IsTelenorSupported = true;
+					}
+					 Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name + " IsTelenorSupported - " + keyValue.Value.IsTelenorSupported);
+				}
+			}
 		}
 	}
-}
+
+	class Country
+	{
+		public string Name {get; set;}
+		public bool IsTelenorSupported { get; set; }
+
+		
+	}
+
+	
+}	
+
+
